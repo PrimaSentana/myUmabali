0d94097dca573332036113a12763dc0fa0e97ce8
PrimaSentana <premasentana123@gmail.com>
2025-12-04 05:03:56 +0800

    fix header and navbar "view profile"

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -83,7 +83,7 @@
              @click.away="openUser = false"
              x-transition
              class="absolute right-0 mt-2 w-40 bg-white shadow-lg rounded-lg border border-gray-100 z-50">
-            <a href="#"
+            <a href="/profile"
               class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 rounded-lg">
               View Profile
             </a>
