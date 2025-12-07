7faca04a605140c5ea10f8b03dfb71b133722176
PrimaSentana <premasentana123@gmail.com>
2025-12-08 01:48:04 +0800

    guarded table = null

--- app/Models/ListingImage.php
+++ app/Models/ListingImage.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ListingImage extends Model
 {
     protected $table = 'listing_images';
+    protected $guarded = [];
 
     public function listings() {
         return $this->belongsTo(Listings::class);
