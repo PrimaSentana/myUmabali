cc46ded37c8751159504698938bb96c27c5b2a6c
PrimaSentana <premasentana123@gmail.com>
2025-12-07 20:55:29 +0800

    make preview and stack file images function form-penginapan

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { imageUpload } from './listings/image-upload';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener('DOMContentLoaded', () => {
+    imageUpload();
+});
