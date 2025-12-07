a7e57a71f0f54202aace742cfca7e55f04b9c3c4
PrimaSentana <premasentana123@gmail.com>
2025-12-08 01:47:32 +0800

    penginapan controller

--- app/Http/Controllers/PenginapanController.php
+++ app/Http/Controllers/PenginapanController.php
@@ -11,65 +11,83 @@ use Illuminate\Support\Facades\Auth;
 
 class PenginapanController extends Controller
 {
+    public function test() { //ngetest azahhh
+        dd(request()->all());
+    }
+    public function index() {
+        $listings = Listings::all();
+        return view('xdashboard', ['listings' => $listings]);
+    }
+    
     public function create() {
         $categories = Category::all();
 
         $facilities = Facility::all();
 
-        return view('forms.form-penginapan', ['categories' => $categories, 'facilities' => $facilities]);
+        return view('listings.create', ['categories' => $categories, 'facilities' => $facilities]);
     }
 
-    public function store() {
-        // $request->validate([
-        //     'title' => ['required', 'min:5'],
-        //     'description' => ['required'],
-        //     'room_count' => ['required'],
-        //     'guest_count' => ['required'],
-        //     'bathroom_count' => ['required'],
-        //     'location_value' => ['required'],
-        //     'category' => ['required'],
-        //     'facilities' => ['array', 'required'],
-        //     'price' => ['required']
-        // ]);
+    public function store(Request $request) {
+        $request->validate([
+            'title' => ['required', 'min:5'],
+            'description' => ['required'],
+            'room_count' => ['required'],
+            'guest_count' => ['required'],
+            'bathroom_count' => ['required'],
+            'location_value' => ['required'],
+            'category' => ['required'],
+            'facilities' => ['array'],
+            'images' => ['array'],
+            'price' => ['required']
+        ]);
 
-        // $listings = Listings::create([
-        //     'title' => request('title'),
-        //     'category_id' => request('category'),
-        //     'description' => request('description'),
-        //     'room_count' => request('room_count'),
-        //     'room_status' => request('room_count'),
-        //     'guest_count' => request('guest_count'),
-        //     'bathroom_count' => request('bathroom_count'),
-        //     'location_value' => request('location_value'),
-        //     'price' => request('price'),
-        //     'user_id' => Auth::id()
-        // ]);
+        $listings = Listings::create([
+            'title' => request('title'),
+            'category_id' => request('category'),
+            'description' => request('description'),
+            'room_count' => request('room_count'),
+            'room_status' => request('room_count'),
+            'guest_count' => request('guest_count'),
+            'bathroom_count' => request('bathroom_count'),
+            'location_value' => request('location_value'),
+            'price' => request('price'),
+            'user_id' => Auth::id()
+        ]);
 
-        // // handle facilities
-        // if(request()->filled('facilities')) {
-        //     $listings->facilities()->sync(request()->facilities);
-        // }
+        // handle facilities
+        if(request()->filled('facilities')) {
+            $listings->facilities()->sync(request()->facilities);
+        }
 
-        // // handle image
-        // if(request()->hasFile('images')) {
-        //     foreach(request()->file('images') as $index => $image) {
-        //         $path = $image->store('listings', 'public');
+        // handle image
+        if(request()->hasFile('images')) {
+            foreach(request()->file('images') as $index => $image) {
+                $path = $image->store('listings', 'public');
 
-        //         ListingImage::create([
-        //             'listing_id' => $listings['id'],
-        //             'image_path' => $path,
-        //             'isCover' => $index === 0, // image pertama
-        //             'isKamar' => $index === 1  // image kedua
-        //         ]);
-        //     }
-        // }
+                ListingImage::create([
+                    'listings_id' => $listings['id'],
+                    'image_path' => $path,
+                    'isCover' => $index === 0, // image pertama
+                    'isKamar' => $index === 1  // image kedua
+                ]);
+            }
+        }
 
-        dd(request()->all());
 
-        // return redirect()->route('listings.index')
-        // ->with('success', 'Penginapan berhasil ditambahkan');
+        return redirect()->route('xdashboard')
+        ->with('success', 'Penginapan berhasil ditambahkan');
     }
     
+    public function show($id){
+        $listings = Listings::find($id);
+        $images = $listings->images()
+        ->where('isCover', false)
+        ->get();
+        $facilities = $listings->facilities()->get();
+
+        return view('listings.show', ['listings' => $listings, 'images' => $images, 'facilities' => $facilities]);
+    }
+
     public function edit() {
         return;
     }
