91207af06498172534f25b9f3df6dfac09c2fd06
PrimaSentana <premasentana123@gmail.com>
2025-12-10 00:34:38 +0800

    wishlist system, style before and after clicked

--- resources/views/components/home-card.blade.php
+++ resources/views/components/home-card.blade.php
@@ -1,13 +1,29 @@
-@props(['id','image_path', 'title', 'price'])
+@props(['id','image_path', 'title', 'price', 'isRelated'])
 
 <a href="/penginapan/{{ $id }}">
-    <div class="rounded-2xl overflow-hidden shadow-md hover:shadow-xl transition bg-white w-55 cursor-pointer">
+    <div {{ $attributes->merge(['class' => 'rounded-2xl overflow-hidden shadow-md hover:shadow-xl transition bg-white w-55 cursor-pointer']) }}>
         <div class="relative">
             <img src="{{ asset('storage/' . $image_path)}}" alt="{{ $title }}" class="h-48 w-full object-cover">
-            {{-- <span class="absolute top-2 left-2 bg-white text-sm font-semibold px-2 py-1 rounded-md shadow">Guest favorite</span> --}}
-            <button class="absolute top-2 right-2 bg-white rounded-full p-2 shadow">
-                <img src="images/heart.png" alt="heart icon" width="20px">
-            </button>
+            {{-- favorite --}}
+            @if($isRelated)
+                <form action="{{ route('listings.xfavorite', $id) }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="listings_id" value="{{ $id }}">
+                    <button type="submit" class="group absolute top-2 left-2 bg-[#eb4c60] w-20 transition text-white text-sm font-semibold px-2 py-1 rounded-md shadow hover:bg-white hover:text-black">
+                        <span class="block group-hover:hidden">Favorited</span>
+                        <span class="hidden group-hover:block">Cancel</span>
+                    </button>
+                </form>
+            @else
+                <form action="{{ route('listings.favorite', $id) }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="listings_id" value="{{ $id }}">
+                    <button type="submit" class="group absolute top-2 left-2 bg-white w-32 hover:bg-[#eb4c60] transition hover:text-white active:bg-[#e72842] text-sm font-semibold px-2 py-1 rounded-md shadow">
+                        <span class="block group-hover:hidden">Add to favorite</span>
+                        <span class="hidden group-hover:block">Favorite</span>
+                    </button>
+                </form>
+            @endif
         </div>
         <div class="p-3">
             <h3 class="font-semibold text-gray-800 text-sm">{{ $title }}</h3>
