838a2afb46379b2ea113cd6f51f31bd349e8878f
PrimaSentana <premasentana123@gmail.com>
2025-12-10 00:33:37 +0800

    minor fix

--- resources/views/xdashboard.blade.php
+++ resources/views/xdashboard.blade.php
@@ -2,15 +2,16 @@
 @section('content')
     <div>
         <h2 class="text-2xl font-bold mb-6">Popular homes</h2>
-            <div class="flex flex-wrap gap-6">
-                @foreach($listings as $listing)
-                    <x-home-card 
-                        :id="$listing->id"
-                        :image_path="$listing->coverImage->image_path"
-                        :title="$listing->title" 
-                        :price="$listing->price"
-                    />
-                @endforeach
-            </div>
+        <div class="flex flex-wrap gap-4">
+            @foreach($listings as $listing)
+                <x-home-card 
+                    :id="$listing->id"
+                    :image_path="$listing->coverImage->image_path"
+                    :title="$listing->title" 
+                    :price="$listing->price"
+                    :isRelated="$user->favorites()->where('listings_id', $listing->id)->exists()"
+                />
+            @endforeach
+        </div>
     </div>
 @endsection
