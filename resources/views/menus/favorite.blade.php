6a87a4afd8ee425a5d860bb729dd8ff49e27be06
PrimaSentana <premasentana123@gmail.com>
2025-12-10 00:34:58 +0800

    menu favorite

---
+++ resources/views/menus/favorite.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.home')
+@section('content')
+    <div>
+        <div>
+            <h1 class="text-4xl font-medium">Favorite</h1>
+            <p>Daftar penginapan favorite anda!</p>
+        </div>
+        <div class="flex flex-wrap gap-8 mt-4">
+            @foreach($listings as $listing)
+                <x-home-card 
+                    :id="$listing->id"
+                    :image_path="$listing->coverImage->image_path"
+                    :title="$listing->title" 
+                    :price="$listing->price"
+                    :isRelated="$user->favorites()->where('listings_id', $listing->id)->exists()"
+                />
+            @endforeach
+        </div>
+    </div>
+@endsection
