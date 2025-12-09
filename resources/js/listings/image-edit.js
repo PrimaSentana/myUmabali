2d24bf8169999767e24ca09489b4ea67bbd0ec3d
PrimaSentana <premasentana123@gmail.com>
2025-12-09 16:55:07 +0800

    fix upload image form edit

--- resources/js/listings/image-edit.js
+++ resources/js/listings/image-edit.js
@@ -1,5 +1,6 @@
 export function imageEdit() {
-    const preview = document.getElementById('image-preview');
+    const previewLama = document.getElementById('image-preview-lama');
+    // const preview = document.getElementById('image-preview-baru')
     const input = document.getElementById('images-edit');
     const removedImages = new Set();
     let selectedFiled = [];
@@ -14,7 +15,44 @@ export function imageEdit() {
         render();
     });
 
-    preview.addEventListener('click', function (e) {
+    function render() {
+        preview.innerHTML = '';
+
+        Array.from(dataTransfer.files).forEach((file, index) => {
+            const url = URL.createObjectURL(file);
+
+            const wrapper = document.createElement('div');
+            wrapper.className = 'relative group';
+
+            wrapper.innerHTML = `
+                <img src="${url}" class="w-full h-32 object-cover rounded-xl">
+                <button type="button" class="text-white font-bold absolute top-2 right-2 rounded-full w-7 h-7">
+                    ✕
+                </button>
+            `;
+
+            wrapper.querySelector('button')
+                .addEventListener('click', () => remove(index));
+
+            preview.appendChild(wrapper);
+        });
+    }
+
+    function remove(index) {
+        const dt = new DataTransfer();
+        Array.from(dataTransfer.files).forEach((f, i) => {
+            if (i !== index) dt.items.add(f);
+        });
+
+        dataTransfer.items.clear();
+        Array.from(dt.files).forEach(f => dataTransfer.items.add(f));
+
+        input.files = dataTransfer.files;
+        render();
+    }
+
+    // image lama
+    previewLama.addEventListener('click', function (e) {
         if(!e.target.classList.contains('remove-image')) return;
 
         const wrapper = e.target.closest('.image-item');
