b774d39875fa253c100715b6038e256844fdf821
PrimaSentana <premasentana123@gmail.com>
2026-01-03 18:10:13 +0800

    relation to get images data for filament

--- app/Models/Penginapan.php
+++ app/Models/Penginapan.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Penginapan extends Model
 {
     protected $table = 'listings';
+
+    public function images() {
+        return $this->hasMany(ListingImage::class, 'listings_id');
+    }
 }
