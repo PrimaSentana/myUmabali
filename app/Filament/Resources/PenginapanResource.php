749c5009ad1e6e9e1694574ca798d2a87df472cb
PrimaSentana <premasentana123@gmail.com>
2026-01-03 18:08:33 +0800

    implement map view in filament PenginapanResource

--- app/Filament/Resources/PenginapanResource.php
+++ app/Filament/Resources/PenginapanResource.php
@@ -5,10 +5,15 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\PenginapanResource\Pages;
 use App\Filament\Resources\PenginapanResource\RelationManagers;
 use App\Models\Penginapan;
+use Dotswan\MapPicker\Fields\Map;
 use Filament\Forms;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -24,7 +29,66 @@ class PenginapanResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('title')
+                ->label('Title Penginapan')
+                ->required()
+                ->maxLength(255),
+                TextInput::make('description')
+                ->label('Deskripsi')
+                ->required()
+                ->maxLength(255),
+                TextInput::make('room_count')
+                ->label('Banyak Kamar')
+                ->numeric()
+                ->required(),
+                TextInput::make('bathroom_count')
+                ->label('Banyak Kamar Mandi')
+                ->numeric()
+                ->required(),
+                TextInput::make('guest_count')
+                ->label('Banyak Tamu')
+                ->numeric()
+                ->required(),
+                TextInput::make('location_value')
+                ->label('Detail Lokasi')
+                ->required()
+                ->maxLength(255),
+
+                Map::make('lokasi_map')
+                ->label('Lokasi Penginapan')
+                ->columnSpanFull() 
+                ->defaultLocation(latitude: -8.409518, longitude: 115.188919) 
+                ->afterStateUpdated(function (Set $set, ?array $state) {
+                    $set('latitude', $state['lat']);
+                    $set('longitude', $state['lng']);
+                })
+                ->afterStateHydrated(function ($state, $record, Set $set) {
+                    if ($record) {
+                        $set('lokasi_map', [
+                            'lat' => $record->latitude,
+                            'lng' => $record->longitude,
+                        ]);
+                    }
+                })
+                ->liveLocation() 
+                ->showMarker() 
+                ->showFullscreenControl() 
+                ->showZoomControl() 
+                ->draggable() 
+                ->tilesUrl("https://tile.openstreetmap.de/{z}/{x}/{y}.png") 
+                ->zoom(15),
+
+                Group::make()
+                ->schema([
+                    TextInput::make('latitude')
+                    ->required()
+                    ->readOnly(),
+                    TextInput::make('longitude')
+                    ->required()
+                    ->readOnly(),
+                ])
+                ->columns(2)
+                ->columnSpanFull()
             ]);
     }
 
@@ -34,6 +98,8 @@ class PenginapanResource extends Resource
             ->columns([
                 TextColumn::make('title')
                 ->searchable(),
+                ImageColumn::make('images.image_path')
+                ->label('Image'),
                 TextColumn::make('description')
                 ->searchable(),
                 TextColumn::make('room_count')
@@ -65,6 +131,11 @@ class PenginapanResource extends Resource
         return [
             //
         ];
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
     }
 
     public static function getPages(): array
